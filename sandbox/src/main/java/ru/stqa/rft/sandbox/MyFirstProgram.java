8ab5715894a83cb195548cd4186a1c6b1807f20f
Kirilovaa <84980576+Kirilovaa@users.noreply.github.com>
2021-06-03 23:27:57 +0300

    Задание №2: Потренироваться использовать функции, классы, объекты и методы

--- sandbox/src/main/java/ru/stqa/rft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/rft/sandbox/MyFirstProgram.java
@@ -1,8 +1,15 @@
 package ru.stqa.rft.sandbox;
 
 public class MyFirstProgram {
-   public static void main (String [] args) {
 
-       System.out.println( "Hello, word!");
-	}
+  public static void main(String[] args) {
+    Point start = new Point(2,3);
+    Point end = new Point(4,5);
+    //AB = √(xb - xa)2 + (yb - ya)2
+    double distance = Math.sqrt(Math.pow((end.a- start.a),2)+Math.pow((end.b- start.b),2));
+
+    System.out.println("Расстояние между точками равно: "+distance);
+
+    }
 }
+
---
+++ sandbox/src/main/java/ru/stqa/rft/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.rft.sandbox;
+
+public class Point {
+
+  public double a;
+  public double b;
+
+  public Point(double a, double b) {
+    this.a = a;
+    this.b = b;
+
+  }
+
+
+}
