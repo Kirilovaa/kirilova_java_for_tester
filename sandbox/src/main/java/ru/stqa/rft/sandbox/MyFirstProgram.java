3ef06813865e047ea91d694a223f2d8d5053248b
Kirilovaa <84980576+Kirilovaa@users.noreply.github.com>
2021-06-04 23:53:23 +0300

    Изменения по заданию 2

--- sandbox/src/main/java/ru/stqa/rft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/rft/sandbox/MyFirstProgram.java
@@ -5,23 +5,17 @@ public class MyFirstProgram {
   public static void main(String[] args) {
 
     Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " +area(s));
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " +s.area());
 
     Rectangle r = new Rectangle(4,6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " +area(r));
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " +r.area());
 
-    Point start = new Point(2,3);
-    Point end = new Point(4,5);
-    //AB = √(xb - xa)2 + (yb - ya)2
-    double distance = Math.sqrt(Math.pow((end.a- start.a),2)+Math.pow((end.b- start.b),2));
-    System.out.println("Расстояние между точками равно: "+distance);
+    distance d = new distance ();
+    System.out.println("Расстояние между точками равно: "+ distance);
 
+    p1.distance(p2);
     }
-  public static double area (Square s) {
-    return s.l * s.l;
-  }
-  public static double area (Rectangle r){
-    return r.a * r.b;
-  }
+
+
 }
 
--- sandbox/src/main/java/ru/stqa/rft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/rft/sandbox/Point.java
@@ -2,12 +2,23 @@ package ru.stqa.rft.sandbox;
 
 public class Point {
 
-  public double a;
-  public double b;
+  public double x;
+  public double y;
 
-  public Point(double a, double b) {
-    this.a = a;
-    this.b = b;
+  public Point p1(double x, double y){
+    this.x = x;
+    this.y = y;
 
+  }
+  public Point p2(double x, double y){
+    this.x = x;
+    this.y = y;
+
+  }
+  public static double distance (Point p1,Point p2){
+    return (Math.sqrt(Math.pow((p2.x- p1.x),2)+Math.pow((p2.y- p1.y),2)));
+    //AB = √(xb - xa)2 + (yb - ya)2
+
+  public double distance(Point p);
   }
 }
