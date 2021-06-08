459a3f1c9d88b4b878f8e2308dc89170fe6a4350
Kirilovaa <84980576+Kirilovaa@users.noreply.github.com>
2021-06-08 19:20:44 +0300

    NEW_Задание №2: Потренироваться использовать функции, классы, объекты и методы

--- sandbox/src/main/java/ru/stqa/rft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/rft/sandbox/Point.java
@@ -5,20 +5,16 @@ public class Point {
   public double x;
   public double y;
 
-  public Point p1(double x, double y){
+  public Point (double x, double y){
     this.x = x;
     this.y = y;
 
   }
-  public Point p2(double x, double y){
-    this.x = x;
-    this.y = y;
+  public double distance (Point p2){
+    return (Math.sqrt(Math.pow((p2.x- this.x),2)+Math.pow((p2.y- this.y),2)));
 
-  }
-  public static double distance (Point p1,Point p2){
-    return (Math.sqrt(Math.pow((p2.x- p1.x),2)+Math.pow((p2.y- p1.y),2)));
-    //AB = √(xb - xa)2 + (yb - ya)2
 
-  public double distance(Point p);
+
+
   }
 }
